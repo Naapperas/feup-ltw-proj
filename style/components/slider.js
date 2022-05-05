abca06e1a7362a1d83dc1870eb5f00170d2ed1bb
toino <joaoapereira21@hotmail.com>
2022-05-05 17:52:29 +0100

    Adjust some styles and separate component files

--- style/components/slider.js
+++ style/components/slider.js
@@ -2,20 +2,43 @@
 
 // Sliders suck.
 
-const getSliderBackgroundSVG = (width, percentage, fill) =>
-    "data:image/svg+xml," +
-    encodeURIComponent(
-        `
+const getSliderBackgroundSVG = (width, percentage, fill, step) => {
+    let circles = "";
+    let progressCircles = "";
+
+    if (step) {
+        for (let i = percentage; i <= 1; i += step)
+            circles += `
+    <circle r='1' cy='3' cx='${10 + (width - 20) * i}' fill='rgb(${fill})' />
+`.trim();
+        for (let i = 0; i < percentage; i += step)
+            progressCircles += `
+    <circle r='1' cy='3' cx='${10 + (width - 20) * i}' fill='black' />
+`.trim();
+    }
+
+    return (
+        "data:image/svg+xml," +
+        encodeURIComponent(
+            `
 <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 ${width} 6'>
+    <mask id="mask">
+        <rect x='0' y='0' width="${width}" height="6" fill="white" />
+        ${progressCircles}
+    </mask>
+
     <rect x='7' y='0' width='${
         (width - 14) * percentage
-    }' height='6' rx='3' ry='3' fill='rgba(${fill})'/>
+    }' height='6' rx='3' ry='3' fill='rgb(${fill})' mask="url(#mask)"/>
     <rect x='8' y='1' width='${
         width - 16
     }' height='4' rx='2' ry='2' fill='rgba(${fill}, .38)'/>
+    ${circles}
 </svg>
 `.trim()
+        )
     );
+};
 
 const getSliderCSS = async (slider) => {
     const background = getSliderBackgroundSVG(
@@ -23,7 +46,8 @@ const getSliderCSS = async (slider) => {
         slider.value / slider.max,
         getComputedStyle(slider).getPropertyValue(
             slider.disabled ? "--tcolor-on-surface" : "--tcolor-primary"
-        )
+        ),
+        slider.step / slider.max
     );
 
     // Fixes flickering on firefox
