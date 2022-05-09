326ef5b59afd6720456855420caf310022a3c03e
Nuno Pereira <nunoafonso2002@gmail.com>
2022-05-09 01:53:59 +0100

    Added login action

---
+++ actions/login.php
@@ -0,0 +1,22 @@
+<?php
+
+    if (strcmp($_SERVER['REQUEST_METHOD'], "POST") !== 0) {
+        header("Location: /index.php");
+        die;
+    }
+
+    session_start();
+
+    if (!isset($_POST['username']) || !isset($_POST['password']) || !isset($_POST['referer'])) {
+        die("Error: some form data isn't set");
+        print_r($_POST);
+    }
+
+    require_once('../database/models/user.php');
+    require_once('../lib/user.php');
+
+    if (userExists($_POST['username'], $_POST['password']))
+        $_SESSION['username'] = User::get(array("name" => $_POST['username']), true);
+
+    header('Location: ' . $_POST['referer']);
+?>
