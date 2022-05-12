7b7228f7a01b53d19ba8105c98754e6c7108b4d8
Nuno Pereira <nunoafonso2002@gmail.com>
2022-05-12 02:52:50 +0100

    Started using components in mainpage

--- mainpage/index.php
+++ mainpage/index.php
@@ -5,16 +5,10 @@ require_once("../templates/components.php");
 ?>
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset="UTF-8" />
-        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>Xau Fome - Login</title>
-
-        <link rel="stylesheet" href="../style/index.css" />
-        <script src="../scripts/components/form.js" defer></script>
-    </head>
-
+    <?php createHead(
+        description: "Home page for XauFome.", 
+        scripts: ["components/form.js"],
+    ); ?>
     <body class="centered medium medium-spacing single column layout">
         <header class="appbar">
             <a href="." class="homepage-link"><h1 class="h6 color logo"></h1></a>
--- templates/components.php
+++ templates/components.php
@@ -17,7 +17,13 @@ enum ButtonType: string {
         <meta charset="UTF-8" />
         <meta http-equiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title><?= $title ?> - Xau Fome</title>
+        <title>
+            <?php if (strcmp($title, "")) { ?>
+            <?= $title ?> - Xau Fome
+            <?php } else { ?>
+                Xau Fome
+            <?php } ?>
+        </title>
 
         <link rel="stylesheet" href="../style/index.css" />
 
