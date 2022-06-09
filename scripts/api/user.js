49ff1cae860a57de3ad201e88300ef2ae303dda6
toino <joaoapereira21@hotmail.com>
2022-06-09 03:42:40 +0100

    Fix user api

--- scripts/api/user.js
+++ scripts/api/user.js
@@ -7,7 +7,7 @@ import { addSnackbar } from '../components/snackbar.js';
 export const fetchUser = async (/** @type {Number} */ userId) => {
 
     const data = {
-        'userId': userId.toString(10),
+        'id': userId.toString(10),
     };
 
     const response = await fetch(`/api/user?${Object.entries(data).map(([k, v]) => `${k}=${v}`).join("&")}`);
